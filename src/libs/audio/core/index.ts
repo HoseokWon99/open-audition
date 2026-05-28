6abd3a27044a9f1380565f7970b4987a7c359153
hoseok <hs1j333@gmail.com>
2026-05-28 09:07:33 +0900

    feat: export audio chain APIs

--- src/libs/audio/core/index.ts
+++ src/libs/audio/core/index.ts
@@ -1,1 +1,6 @@
 export { AudioNodeManager } from "./audioNodeManager";
+export { createAudioChain } from "./audioChain";
+export type { AudioChain } from "./audioChain";
+export type { AudioEffect } from "./audioEffect";
+export { createClipChain, createMasterChain, createTrackChain } from "./mixChains";
+export type { ClipChainOptions, MasterChainOptions, TrackChainOptions } from "./mixChains";
